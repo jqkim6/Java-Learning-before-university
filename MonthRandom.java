e3251dea500fd73979189e760679d73ef4cbed9f
jqkim6 <50802275+jqkim6@users.noreply.github.com>
2019-05-24 02:31:06 +0800

    Add files via upload

---
+++ MonthRandom.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author jiangqiang
+ */
+public class MonthRandom {
+    public static void main(String[] args){
+    int monthRandom = (int) (Math.random() * 12);
+    switch (monthRandom){
+        case 1: System.out.println("January");break;
+        case 2: System.out.println("February");break;
+        case 3: System.out.println("March");break;
+        case 4: System.out.println("April");break;
+        case 5: System.out.println("May");break;
+        case 6: System.out.println("June");break;
+        case 7: System.out.println("July");break;
+        case 8: System.out.println("August");break;
+        case 9: System.out.println("September");break;
+        case 10: System.out.println("October");break;
+        case 11: System.out.println("November");break;
+        case 12: System.out.println("December");break;
+    }
+    }
+}
